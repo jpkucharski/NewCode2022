8bf4c299725d5fd1f2ef2a5789fd7aefca09431e
unknown <j.p.kucharski@gmail.com>
2022-01-19 22:16:22 +0100

    Facade

---
+++ DesignePaterns/Facade/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        FacadeShapeMaker shapeMaker = new FacadeShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
